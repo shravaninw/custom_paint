dbb53d85ab9cd655854b0fd1e7705773830d8e3c
shravaninw <shravani.s@nativewit.in>
2021-11-11 17:14:43 +0530

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -99,12 +99,15 @@ class LPSCustomPainter extends CustomPainter {
       ..strokeWidth = 1;
 
     Path path0 = Path();
-    path0.moveTo(0, size.height * 0.6604333);
-    path0.quadraticBezierTo(size.width * 0.2755474, size.height * 0.6651083,
-        size.width * 0.3673966, size.height * 0.6666667);
-    path0.quadraticBezierTo(size.width * 0.5098054, size.height * 0.6979333,
-        size.width * 0.6694161, 0);
+    path0.moveTo(0, 0);
+    path0.lineTo(0, size.height * 0.8300000);
+    path0.quadraticBezierTo(size.width * 0.1929927, size.height * 0.8351000,
+        size.width * 0.2738929, size.height * 0.8326000);
+    path0.quadraticBezierTo(size.width * 0.3537470, size.height * 0.8448000,
+        size.width * 0.4160584, size.height * 0.6633333);
+    path0.lineTo(size.width * 0.6666180, 0);
     path0.lineTo(0, 0);
+    path0.close();
 
     canvas.drawPath(path0, paint0);
   }
@@ -124,18 +127,22 @@ class RPSCustomPainter extends CustomPainter {
       ..strokeWidth = 1;
 
     Path path0 = Path();
-    path0.moveTo(size.width * 0.7956204, 0);
-    path0.quadraticBezierTo(size.width * 0.5188564, size.height * 0.5616667,
-        size.width * 0.4671533, size.height * 0.7700000);
+    path0.moveTo(size.width * 0.7992701, 0);
+    path0.quadraticBezierTo(size.width * 0.5447932, size.height * 0.6352000,
+        size.width * 0.4604623, size.height * 0.8421000);
+    path0.quadraticBezierTo(size.width * 0.4225304, size.height * 0.9773000,
+        size.width * 0.5246472, size.height * 1.0037667);
+    path0.quadraticBezierTo(size.width * 0.8090024, size.height * 1.0100000,
+        size.width * 0.9093674, size.height * 1.0100000);
     path0.cubicTo(
-        size.width * 0.4172749,
+        size.width * 0.9221411,
+        size.height * 1.0162667,
+        size.width * 0.9413139,
         size.height * 0.9900000,
-        size.width * 0.8649635,
-        size.height * 0.9000000,
-        size.width * 0.9197080,
-        size.height * 0.9100000);
-    path0.quadraticBezierTo(size.width * 0.9872263, size.height * 0.9100000,
-        size.width, size.height * 0.8200000);
+        size.width * 0.9498297,
+        size.height * 0.9771000);
+    path0.quadraticBezierTo(size.width * 0.9601703, size.height * 0.9600000,
+        size.width * 1.0015328, size.height * 0.8837667);
     path0.lineTo(size.width, 0);
 
     canvas.drawPath(path0, paint0);
@@ -151,18 +158,18 @@ class TPSCustomPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     Paint paint0 = Paint()
-      ..color = const Color.fromARGB(133, 13, 77, 179)
+      ..color = const Color.fromARGB(110, 33, 150, 243)
       ..style = PaintingStyle.fill
       ..strokeWidth = 1;
 
     Path path0 = Path();
-    path0.moveTo(size.width * 0.7946959, 0);
-    path0.quadraticBezierTo(size.width * 0.6482238, size.height * 0.3219667,
-        size.width * 0.5916545, size.height * 0.4478000);
-    path0.quadraticBezierTo(size.width * 0.4885401, size.height * 0.5521333,
-        size.width * 0.4773236, size.height * 0.3684667);
-    path0.lineTo(size.width * 0.6123114, 0);
-    path0.lineTo(size.width * 0.7946959, 0);
+    path0.moveTo(size.width * 0.8012652, 0);
+    path0.quadraticBezierTo(size.width * 0.6584672, size.height * 0.3808000,
+        size.width * 0.6041363, size.height * 0.5049667);
+    path0.quadraticBezierTo(size.width * 0.4800000, size.height * 0.5536333,
+        size.width * 0.4428224, size.height * 0.3700000);
+    path0.lineTo(size.width * 0.5717762, size.height * 0.0033333);
+    path0.lineTo(size.width * 0.8012652, 0);
     path0.close();
 
     canvas.drawPath(path0, paint0);
